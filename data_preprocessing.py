4b3c5da21072014201abb40f364130ea307b095b
RMukeshR <mkram0322@gmail.com>
2024-10-14 01:48:05 +0530

    data

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -7,3 +7,6 @@ df_test = open("TestData","r", encoding= "latin-1").read()
 df_pos_list = [i for i in df_pos.split("\n") if len(i) >= 2]
 df_neg_list = [i for i in df_neg.split("\n") if len(i) >= 2]
 df_test_list = [i for i in df_test.split("\n") if len(i) >= 2]
+
+
+print(df_pos_list)
