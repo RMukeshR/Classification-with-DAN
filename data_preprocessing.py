e74f06092e72e9a58b4b7624b421e4dcad78f346
RMukeshR <mkram0322@gmail.com>
2024-10-14 13:09:08 +0530

    similarity check

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,5 +1,29 @@
+## Read data
+
+df_pos = open("Train.pos","r", encoding= "latin-1").read()
+df_neg = open("Train.neg","r", encoding= "latin-1").read()
+df_test = open("TestData","r", encoding= "latin-1").read()
+
+df_pos_list = [i for i in df_pos.split("\n") if len(i) >= 2]
+df_neg_list = [i for i in df_neg.split("\n") if len(i) >= 2]
+df_test_list = [i for i in df_test.split("\n") if len(i) >= 2]
+
+
 import gensim.downloader as api
+from gensim.models import KeyedVectors
+
 
 # Load and save pre-trained Word2Vec model
-word2vec_model = api.load("word2vec-google-news-300")
-word2vec_model.save("word2vec_model.bin")
+# word2vec_model = api.load("word2vec-google-news-300")
+# word2vec_model.save("word2vec_model.bin")
+
+
+# Load the saved Word2Vec model from file
+loaded_model = KeyedVectors.load("word2vec_model.bin")
+
+# Find similar words to "apple" using the loaded model
+similar_words = loaded_model.most_similar("apple", topn=5)
+
+print("Words similar to 'apple':")
+for word, similarity in similar_words:
+    print(f"{word}: {similarity:.4f}")
