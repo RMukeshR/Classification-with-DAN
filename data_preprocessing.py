5bbd2e65fce744013b1b527c79674829ca85b3be
RMukeshR <mkram0322@gmail.com>
2024-10-14 17:39:21 +0530

    embeddings

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -9,7 +9,7 @@ df_neg_list = [i for i in df_neg.split("\n") if len(i) >= 2]
 df_test_list = [i for i in df_test.split("\n") if len(i) >= 2]
 
 
-import gensim.downloader as api
+# import gensim.downloader as api
 from gensim.models import KeyedVectors
 
 
@@ -18,12 +18,33 @@ from gensim.models import KeyedVectors
 # word2vec_model.save("word2vec_model.bin")
 
 
+# Load and save the pre-trained GloVe model
+# glove_model = api.load("glove-wiki-gigaword-300")
+# glove_model.save("glove_model.bin")
+
+
+# # Load and save the pre-trained FastText model
+# fasttext_model = api.load("fasttext-wiki-news-subwords-300")
+# fasttext_model.save("fasttext_model.bin")
+
 # Load the saved Word2Vec model from file
-loaded_model = KeyedVectors.load("word2vec_model.bin")
+loaded_woord2vec_model = KeyedVectors.load("word2vec_model.bin")
+loaded_glove_model = KeyedVectors.load("glove_model.bin")
+loaded_fasttext_model = KeyedVectors.load("fasttext_model.bin")
+
 
 # Find similar words to "apple" using the loaded model
-similar_words = loaded_model.most_similar("apple", topn=5)
+similar_words_word2vec = loaded_woord2vec_model.most_similar("apple", topn=5)
+similar_words_glove = loaded_glove_model.most_similar("apple", topn=5)
+similar_words_fasttext = loaded_fasttext_model.most_similar("apple", topn=5)
 
-print("Words similar to 'apple':")
-for word, similarity in similar_words:
+
+# print("Words similar to 'apple':")
+for word, similarity in similar_words_word2vec:
+    print(f"{word}: {similarity:.4f}")
+
+for word, similarity in similar_words_glove:
+    print(f"{word}: {similarity:.4f}")
+
+for word, similarity in similar_words_fasttext:
     print(f"{word}: {similarity:.4f}")
